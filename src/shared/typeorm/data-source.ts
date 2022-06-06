1ae7860489b37669f98f7b220d9d9a7c8c14e961
Luiz Fernando <devfernandoof@gmail.com>
2022-06-06 18:09:35 -0300

    feat: connect to db using env variables

--- src/shared/typeorm/data-source.ts
+++ src/shared/typeorm/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 const appDataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'docker',
-  password: 'ignite',
-  database: 'qlx',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   migrations: ['./src/shared/infra/http/migrations/**.ts'],
   entities: ['./src/modules/**/entities/*.ts'],
   synchronize: false,
