06c73a5c58137071951cdcec5545bd44bdd5b95d
Luiz Fernando <devfernandoof@gmail.com>
2022-06-18 12:30:09 -0300

    test(advertisements): testing create resolver and create use case

---
+++ src/modules/advertisements/useCases/createAdvertisement/create-advertisement.resolver.test.ts
@@ -0,0 +1,62 @@
+import { DataSource } from 'typeorm';
+import { gCall } from '../../../../test-utils/g-call';
+import { testDataSource } from '../../../../test-utils/test-data-source';
+import { AdvertisementRepository } from '../../infra/repositories/advertisement.repository';
+import { CategoryRepository } from '../../infra/repositories/category.repository';
+import { AdvertisementRepositoryModel } from '../../repositories/advertisement-repository.model';
+import { CategoryRepositoryModel } from '../../repositories/category-repository.model';
+
+let dataSource: DataSource;
+let categoryRepository: CategoryRepositoryModel;
+let advertisementRepository: AdvertisementRepositoryModel;
+
+beforeAll(async () => {
+  dataSource = await testDataSource();
+  categoryRepository = new CategoryRepository(dataSource);
+  advertisementRepository = new AdvertisementRepository(dataSource);
+});
+
+afterAll(async () => {
+  dataSource.destroy();
+});
+
+const createAdvertisementMutation = `
+  mutation CreateAdvertisement($data: CreateAdvertisementDTO!) {
+    createAdvertisement(data: $data) {
+      id
+    }
+  }
+`;
+
+describe('Create Advertisement Resolver', () => {
+  it('should be able to create a new advertisement', async () => {
+    const category = await categoryRepository.findByDescription('Roupas');
+    const data = { title: 'Camisa do Brasil', address: 'Rio de Janeiro', categoryId: category?.id };
+    const response = await gCall({
+      source: createAdvertisementMutation,
+      variableValues: {
+        data,
+      },
+    });
+
+    expect(response.data?.createAdvertisement).toHaveProperty('id');
+
+    const entity = await advertisementRepository.findById(response.data?.createAdvertisement.id);
+
+    expect(entity).toBeDefined();
+  });
+
+  it('should not be able to create a new advertisement without category id', async () => {
+    const data = { title: 'Camisa do Brasil', address: 'Rio de Janeiro' };
+    const response = await gCall({
+      source: createAdvertisementMutation,
+      variableValues: {
+        data,
+      },
+    });
+
+    expect(response.errors).toBeDefined();
+
+    expect(response.errors?.length).toEqual(1);
+  });
+});
---
+++ src/modules/advertisements/useCases/createAdvertisement/create-advertisement.use-case.test.ts
@@ -0,0 +1,41 @@
+import { AppError } from '../../../../shared/error/app.error';
+import { AdvertisementInMemoryRepository } from '../../infra/repositories/in-memory/advertisement-in-memory.repository';
+import {
+  CreateAdvertisementUseCase,
+  ICreateAdvertisementModel,
+} from './create-advertisement.use-case';
+
+describe('Create advertisement', () => {
+  let advertisementInMemoryRepository: AdvertisementInMemoryRepository;
+  let createAdvertisementUseCase: CreateAdvertisementUseCase;
+
+  beforeEach(() => {
+    advertisementInMemoryRepository = new AdvertisementInMemoryRepository();
+    createAdvertisementUseCase = new CreateAdvertisementUseCase(advertisementInMemoryRepository);
+  });
+
+  it('should be able to create a new advertisement', async () => {
+    const createAdvertisementModel: ICreateAdvertisementModel = {
+      address: 'Rua São Clemente 160',
+      title: 'Salão de festas',
+      categoryId: '1',
+      description: '100m com churrasqueira e piscina.',
+    };
+
+    const entity = await createAdvertisementUseCase.execute(createAdvertisementModel);
+
+    expect(entity).toBeTruthy();
+  });
+
+  it('should not be able to create a new advertisement with empty fields', async () => {
+    const createAdvertisementModel: ICreateAdvertisementModel = {
+      address: 'Rua São Clemente 160',
+      title: '',
+      categoryId: '',
+    };
+
+    await expect(createAdvertisementUseCase.execute(createAdvertisementModel)).rejects.toEqual(
+      new AppError('Fill all required fields.'),
+    );
+  });
+});
